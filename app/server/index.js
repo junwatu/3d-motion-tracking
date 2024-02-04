399dd3d1fc9ba9aa29e57d174616e347437864bd
Equan P <equan.p@gmail.com>
2024-02-04 10:49:52 +0700

    detect undefined port. restart if not detect it

--- app/server/index.js
+++ app/server/index.js
@@ -74,7 +74,7 @@ function parseSensorData(data) {
 const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 115200 });
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
-if (process.env.TRACKING_DEMO === "OK") {
+if (process.env.TRACKING_DEMO === "OK" && port?.port === undefined) {
 	(async () => {
 		try {
 			await broadcastDemoData();
